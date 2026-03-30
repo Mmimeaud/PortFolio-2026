38ce5ec8782b883a2c65947ef7e3b6a3077636ce
Mmimeaud <mmimeaud@gmail.com>
2026-03-30 16:58:27 +0200

    MAJ Responsive + lien

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
+
+/*console.log("--- TEST DE CHARGEMENT ---");
+alert("Le JS est là !");
+window.onerror = function(msg) {
+  console.log("Erreur détectée : " + msg);
+};*/
+
+
+
 // NAV BAR TOOGLE MENU 
+
+
 
 const toggleMenuBtn = document.querySelector("#menu-toggler");
 const toggleMenuImg = document.querySelector("#menu-toggler img");
 const mainNavlist = document.querySelector("#main-navList");
 const menuLinks = document.querySelectorAll("#main-navList a");
-   console.log(mainNavlist);
 
-[toggleMenuBtn, ...menuLinks].forEach(element => element.addEventListener("click", toggleNav));
+
+if (toggleMenuBtn && mainNavlist) {
+    toggleMenuBtn.addEventListener("click", toggleNav);
+
+    menuLinks.forEach(link => {
+        link.addEventListener("click", toggleNav);
+    });
+} else {
+    console.log("Menu non présent sur cette page.");
+}
 
 function toggleNav(){
     console.log(mainNavlist)
@@ -26,27 +45,71 @@ if(mainNavlist.classList.contains("hidden")){
 
 }
 
-// BOUTON PORTCARTES 
+// BOUTON SUIVANT PRECEDENT ALL PAGES 
+const AllbtnLeft = document.querySelectorAll(".btn-left-js");
+const AllbtnRight = document.querySelectorAll(".btn-right-js");
 
-const btnLeft = document.getElementById("btn-left")
-const btnRight = document.getElementById("btn-right")
-const rail = document.getElementById('rail')
-console.log(rail)
-btnLeft.addEventListener("click", clickLeft)
-btnRight.addEventListener("click", clickRight)
 
-function clickLeft(){
-    if(btnLeft){
-        rail.scrollLeft -= rail.offsetWidth; 
-        console.log('Coucou le bouton gauche')
-    }else{console.log("error")}
-   
-}
+// FONCTION BOUTON 
+AllbtnLeft.forEach(btnL => {
+    btnL.addEventListener("click", () => {
+        const section = btnL.closest('section');
+        const rail = section.querySelector(".rail");
 
-function clickRight(){
-  
-    if(btnRight){
-        rail.scrollLeft += rail.offsetWidth; 
-             console.log('Coucou le bouton droit')
-    }else{console.log("error")}
-}
+        if (!rail) {
+            console.log("Rail pas trouvé")
+            return
+
+        } else
+            rail.scrollBy({ left: -rail.clientWidth, behavior: "smooth" });
+
+    })
+})
+
+AllbtnRight.forEach(btnR => {
+    btnR.addEventListener("click", () => {
+        const section = btnR.closest('section')
+        const rail = section.querySelector('.rail')
+
+        if (!rail) {
+            console.log("rail pas toruvé ")
+            return
+
+
+        } 
+        
+                const isAtEnd = rail.scrollLeft + rail.clientWidth >= rail.scrollWidth - 1;
+        if (isAtEnd) {
+            rail.scrollTo({ left: 0, behavior: "instant" }); // Remet la derniere image de droite vers image de gauche 
+        } else {
+            rail.scrollBy({ left: +rail.clientWidth, behavior: "smooth" });
+        }
+    })
+})
+
+// FONCTION POUR LES DOTS 
+//(fonction Arrow)
+const updateDots = (rail) => {
+const section = rail.closest('section')
+const dots = section.querySelectorAll('.dot'); 
+const activeIndex = Math.round(rail.scrollLeft/ rail.clientWidth); 
+
+dots.forEach(dot => {
+    const dotIndex = parseInt(dot.dataset.index); // Verifie que c'est un nombre 
+
+        if (dotIndex === activeIndex) {
+            dot.classList.add('bg-pink-eclat', 'w-6');      // dot actif → colorié + large
+            dot.classList.remove('bg-gray-300', 'w-3');
+        } else {
+            dot.classList.add('bg-gray-300', 'w-3');         // dot inactif → gris + petit
+            dot.classList.remove('bg-pink-eclat', 'w-6');
+        }
+
+})
+}; 
+
+// APPEL DE LA FONCTION 
+document.querySelectorAll('.rail').forEach(rail => {
+    rail.addEventListener('scroll', () => updateDots(rail))
+    updateDots(rail)
+}); 
